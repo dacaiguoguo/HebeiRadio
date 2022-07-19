1b02e75b6fcb6b60adb757404c85c7a6bce8e463
sunyanguo <dacaiguoguo@163.com>
2022-07-19 10:09:05 +0800

    优化播放器

--- JiTingPlayerOC/JiTingPlayerOC/ViewController.m
+++ JiTingPlayerOC/JiTingPlayerOC/ViewController.m
@@ -62,16 +62,19 @@
     self.contentWebView.opaque = NO;
     self.contentWebView.scrollView.backgroundColor = UIColor.darkGrayColor;
     [self.timer invalidate];
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:1 repeats:YES block:^(NSTimer * _Nonnull timer) {
-        [self.contentWebView evaluateJavaScript:@"window.webkit.messageHandlers.lvJSCallNativeHandler.postMessage(`${document.getElementsByTagName('video')[0].currentTime}`)"
-                              completionHandler:^(id _Nullable obj, NSError * _Nullable error) {
-            NSLog(@"%@", obj);
-        }];
-    }];
+//    self.timer = [NSTimer scheduledTimerWithTimeInterval:1 repeats:YES block:^(NSTimer * _Nonnull timer) {
+//        [self.contentWebView evaluateJavaScript:@"var temp = window.webkit.messageHandlers.lvJSCallNativeHandler.postMessage(`${document.getElementsByTagName('video')[0].currentTime}`)"
+//                              completionHandler:^(id _Nullable obj, NSError * _Nullable error) {
+////            NSLog(@"completionHandler:%@", obj);
+//        }];
+//    }];
+    // document.getElementsByTagName('video')[0].currentTime = 120; 可以直接修改
+    // 可以用设置src来修改播放的文件，这样就不用更换的时候就重新创建webview了
+    // document.getElementsByTagName('video')[0].src = "file:///Users/sunyanguo/Library/Developer/CoreSimulator/Devices/9856E35E-2A9F-4A9D-8B19-5EBD7C9D5CB8/data/Containers/Data/Application/C37EFEEE-F99A-4C7B-A9A6-A7C467B0AD46/Documents/59534E492EF1981126AE865C6BC8C569C2D600B3361B2CAF6F770E470320C1F6.mp4"
 }
 
 - (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message {
-    // NSLog(@"didReceiveScriptMessage:%@", message.body);
+    NSLog(@"didReceiveScriptMessage:%@", message.body);
     NSString *bodys = message.body;
     NSURL *destUrl = [NSUserDefaults.standardUserDefaults URLForKey:@"destUrl"];
     NSURLComponents *coms = [NSURLComponents componentsWithURL:destUrl resolvingAgainstBaseURL:NO];
